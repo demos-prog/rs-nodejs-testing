d2d63dc910fcfd0559383a29435f85fbfb23b6d7
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-10-03 11:37:03 +0300

    07 mocking lib api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,28 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    expect(
+      axios.create({
+        baseURL: 'https://jsonplaceholder.typicode.com',
+      }),
+    ).toBeDefined();
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const response = await throttledGetDataFromApi('users');
+    expect(response).toBeDefined();
+    expect(Array.isArray(response)).toBe(true);
+    expect(response.length).toBeGreaterThan(0);
+    expect(response[0]).toMatchObject({});
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const response = await throttledGetDataFromApi('users');
+    expect(response).toBeDefined();
+    expect(Array.isArray(response)).toBe(true);
+    expect(response.length).toBeGreaterThan(0);
+    expect(response[0]).toMatchObject({});
   });
 });
